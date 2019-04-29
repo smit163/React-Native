5a9fcd85b03af6e908e6a7d6a3594d732f00bea4
Srinivasa Rao Gutha <32850375+srinivasaraog@users.noreply.github.com>
2019-04-29 13:18:39 -0700

    Add files via upload

---
+++ index.ios.js
@@ -0,0 +1,36 @@
+/**
+ * @format
+ */
+
+import { AppRegistry } from 'react-native';
+import { name as appName } from './app.json';
+import React, { Component } from 'react';
+
+
+
+import { ApolloClient } from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+import { HttpLink, InMemoryCache } from 'apollo-boost';
+import { NETWORK_INTERFACE } from './src/config/api';
+import App from './src/App';
+
+// Create the client as outlined in the setup guide
+const client = new ApolloClient({
+    link: new HttpLink({ uri: NETWORK_INTERFACE }),
+    cache: new InMemoryCache()
+});
+
+class Home extends Component {
+    render() {
+        return (
+            <ApolloProvider client={client}>
+                <App />
+            </ApolloProvider>
+        );
+    }
+}
+
+
+AppRegistry.registerComponent(appName, () => Home);
+
+export default Home;
