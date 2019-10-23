478c56fa20115c42c61a711992cb1ce1b393434b
smit163 <smitpateldev0010@gmail.com>
2019-10-23 23:41:22 +0530

    unit test case errors solved

--- __tests__/src/components/app.test.tsx
+++ __tests__/src/components/app.test.tsx
@@ -5,17 +5,11 @@ import App from '../../../src/App'
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
-import ApolloClient from 'apollo-client';
-import { NETWORK_INTERFACE } from '../../../src/config/api';
-import { HttpLink, InMemoryCache } from 'apollo-boost';
+import { createMockClient } from 'mock-apollo-client';
 import toJson from 'enzyme-to-json';
 
 it('renders correctly11', () => {
-    const client = new ApolloClient({
-        link: new HttpLink({ uri: NETWORK_INTERFACE }),
-        cache: new InMemoryCache()
-    });
-    const tree = renderer.create(<App />);
-
+    const client = createMockClient();
+    const tree = renderer.create(<ApolloProvider client={client}><App /></ApolloProvider>);
     expect(toJson(tree)).toMatchSnapshot();
 });
